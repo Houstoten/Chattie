563afb09a444cf1eccc52084ad0a39a76b1c6c45
Houstoten <vanogus@gmail.com>
2020-07-22 23:05:15 +0300

    small fixes

--- src/controls_components/chat_components/Header.tsx
+++ src/controls_components/chat_components/Header.tsx
@@ -12,8 +12,8 @@ function Header(props: HeaderData) {
         <div className="headerWrapper">
             <div className="chatSummary">
                 <div className="chatName">Chattie</div>
-                <div className="chatParticipants"><i className="fas fa-users" style={{ paddingRight: "5px" }}></i>{props.userCount} participants</div>
-                <div className="chatMessages"><i className="fas fa-envelope-open" style={{ paddingRight: "5px" }}></i>{props.messagesCount} messages</div>
+                <div className="chatParticipants"><i className="fas fa-users" style={{ paddingRight: "5px" }}></i>{props.userCount}</div>
+                <div className="chatMessages"><i className="fas fa-envelope-open" style={{ paddingRight: "5px" }}></i>{props.messagesCount}</div>
             </div>
             <div className="chatLast">last message {new Date().getTime() - props.lastMessage.getTime() < (1000 * 60 * 60 * 24)
                 ? "today at " + props.lastMessage.toLocaleTimeString()
